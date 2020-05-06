0f165d9f5c339d771a689d3a169a13775d2c58f4
Anastasia Pertsovsky <62279421+Anastasia2512@users.noreply.github.com>
2020-05-06 12:42:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace homeWork_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // length of the arrey
+            Console.WriteLine("Hello Enter a number of elements :");
+            int lengthOfArray = Convert.ToInt32(Console.ReadLine());
+
+            // number to multiply.
+            Console.WriteLine(" Enter secend number :");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            // initialize array
+            int[] arr = new int[lengthOfArray];
+
+            Console.WriteLine($"Number of elements is : {lengthOfArray} \n number to multiply by :{num}");
+            
+            for(int i=0; i<arr.Length; i++)
+            {
+                arr[i] = i * num;
+                Console.Write(arr[i] +", ");
+            }
+
+
+
+        }
+    }
+}
