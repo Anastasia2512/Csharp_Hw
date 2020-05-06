67e12d28be5d6f13863d3cf4f668ce37b616e7c5
Anastasia Pertsovsky <62279421+Anastasia2512@users.noreply.github.com>
2020-05-06 12:28:06 +0300

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classTesk_lssn_5
+{
+    class Car
+    {
+		private int speed;
+
+		public int Speed
+		{
+			get { return speed; }
+			set { speed = value; }
+		}
+
+		public int GetFester(int newSpeed)
+		{
+			for (int i = 0; i < newSpeed; i++)
+			{
+				Speed++;
+			}
+			return Speed;
+		}
+
+		public int GetSlower(int newSpeed)
+		{
+			for (int i = 0; i < newSpeed; i++)
+			{
+				Speed--;
+			}
+			return Speed;
+		}
+
+		public string Print()
+		{
+			return $"your spead was : {Speed}";
+		}
+
+		public string Stop()
+		{
+			return $"you stopted and your spead was : {Speed}";
+		}
+
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,29 +1,67 @@
 ﻿using System;
+using System.Diagnostics;
 
-namespace homeWork.cs
+namespace classTesk_lssn_5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // length of the arrey
-            Console.WriteLine("Hello Enter a number of elements :");
-            int lengthOfArray = Convert.ToInt32(Console.ReadLine());
 
-            // number to multiply.
-            Console.WriteLine(" Enter secend number :");
-            int num = Convert.ToInt32(Console.ReadLine());
+            //q1
+               Cookie co1 = new Cookie();
+               co1.Gluten = true;
+               co1.Suger = 4;
+               co1.Flour = 0.400d;
+               co1.NumOfEggs = 3;
 
-            // initialize array
-            int[] arr = new int[lengthOfArray];
+               Cookie co2 = new Cookie();
+               co2.Gluten = false;
+               co2.Suger = 5;
+               co2.Flour = 0.400d;
+               co2.NumOfEggs = 4;
 
-            Console.WriteLine($"Number of elements is : {lengthOfArray} \n number to multiply by :{num}");
+               Cookie co3 = new Cookie();
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-                arr[i] = i * num;
-                Console.Write(arr[i] + ", ");
-            }
+
+               Console.WriteLine(printDetails(co1));
+               Console.WriteLine(printDetails(co2));
+               co1.Suger = 6;
+
+               Console.WriteLine(printDetails(co1));
+               Console.WriteLine(printDetails(co2));
+               co1 = co2;
+               co2.NumOfEggs = 5;
+
+               Console.WriteLine(printDetails(co1));
+               Console.WriteLine(printDetails(co2));
+               
+            //q2
+            Car car1 = new Car();
+            car1.Speed = 55;
+            Console.WriteLine($"{car1.Print()}");
+
+            Console.WriteLine("Enter How fester do you like to drive?");
+            int newSpeed = Convert.ToInt32(Console.ReadLine());
+            car1.GetFester(newSpeed);
+            Console.WriteLine($"{car1.Print()}");
+            car1.GetSlower(2);
+            Console.WriteLine($"{car1.Print()}");
+            Console.WriteLine($"{car1.Stop()}");
+
+
+        }
+
+        public static string printDetails(Cookie c)
+        {
+            string res = "Details of cookie:\n";
+            res += $"Gluten: {c.Gluten}\n";
+            res += $"number of eggs: {c.NumOfEggs}\n";
+            res += $"Suger: {c.Suger}\n";
+            res += $"Flour: {c.Flour}\n";
+
+            return res;
+
         }
     }
 }
